3811cefb6e491e100a2debd24041099b0a9da521
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-05 21:33:30 -0400

    Componente Contacto header listo
    se encuentra en la parte superior del header y lo complementa.

---
+++ Components/ContactoH.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Style/ContactoH.less';
+
+export default function ContactoH() {
+  return (
+    <section className='SContactoH'>
+        <div className='SContactoH__d1'>
+            <i class='bx bx-phone'></i>
+            <a href=""><span>Servicio al Cliente: </span>+1(809)-789-8098</a>
+        </div>
+        <div className='SContactoH__d2'>
+            <div className='SContactoH__d2_Email'>
+                <i class='bx bx-envelope' ></i>
+                <a href="mailto:"><span>Email: </span>Company@gmail.com</a>
+            </div>
+            <div className='SContactoH__d2_IconoRed'>
+                <a href=""><i class='bx bxl-instagram' ></i></a>
+                <a href=""><i class='bx bxl-linkedin' ></i></a>
+                <a href=""><i class='bx bxl-twitter'></i></a>
+            </div>
+        </div>
+    </section>
+  )
+}
