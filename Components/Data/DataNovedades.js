d9e565fd103bab521d23cc8c0b5f21a095777493
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-10 12:12:33 -0400

    Se agrego el Array de datos del Novedades.

---
+++ Components/Data/DataNovedades.js
@@ -0,0 +1,27 @@
+const DataNovedades = [
+    {
+        img: 'https://ogtic.gob.do/_next/image?url=https%3A%2F%2Fwp.ogtic.gob.do%2Fwp-content%2Fuploads%2F2025%2F02%2FApertura.jpeg&w=1920&q=75',
+        imgAlt: 'Imagen del nuevo Punto GOB en San Cristóbal',
+        title: 'Presidente Luis Abinader inaugura un nuevo Punto GOB en San Cristóbal',
+        descripcion: '',
+    },
+    {
+        img: 'https://ogtic.gob.do/_next/image?url=https%3A%2F%2Fwp.ogtic.gob.do%2Fwp-content%2Fuploads%2F2024%2F11%2FPuntoGOBSancristobal.jpeg&w=1920&q=75',
+        imgAlt: 'Imagen del Punto GOB de San Cristóbal',
+        title: 'OGTIC expande los Puntos GOB a San Cristóbal',
+        descripcion: '',
+    },
+    {
+        img: 'https://ogtic.gob.do/_next/image?url=https%3A%2F%2Fwp.ogtic.gob.do%2Fwp-content%2Fuploads%2F2024%2F11%2FPuntoGOBSancristobal.jpeg&w=1920&q=75',
+        imgAlt: 'Imagen del Punto GOB de San Cristóbal',
+        title: 'OGTIC expande los Puntos GOB a San Cristóbal',
+        descripcion: '',
+    },
+    {
+        img: 'https://ogtic.gob.do/_next/image?url=https%3A%2F%2Fwp.ogtic.gob.do%2Fwp-content%2Fuploads%2F2024%2F11%2FPuntoGOBSancristobal.jpeg&w=1920&q=75',
+        imgAlt: 'Imagen del Punto GOB de San Cristóbal',
+        title: 'OGTIC expande los Puntos GOB a San Cristóbal',
+        descripcion: '',
+    },
+]
+export default DataNovedades;
