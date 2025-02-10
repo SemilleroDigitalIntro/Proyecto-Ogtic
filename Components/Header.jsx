a7985a6cede494c2b44cbd4bf046bc839e7bc878
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-10 19:00:46 -0400

    Se agrego una navegacion dinamica

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import './Style/Header.less';
 
+
+let Nav;
 export default function Header() {
+
+
+  const Servicios = () =>{
+    scroll(0,600)
+  };
+  const Novedades = () =>{
+    scroll(0,1000)
+  };
+
+
+  const Sobrenosotros = () =>{
+    scroll(0,1500)
+  };
+
+  const Home = () =>{
+    scroll(0,0)
+  };
+
   return (
     <section className='Sheader'>
         <div className='Sheader__logo'>
 
         </div>
         <nav className='Sheader__Nav'>
-          <a href="">Home</a>
-          <a href="">Sobre nosotros</a>
-          <a href="">Servicios</a>
-          <a href="">Novedades</a>
+          <a onClick={Home}>Home</a>
+          <a onClick={Servicios}>Servicios</a>
+          <a onClick={Novedades}>Novedades</a>
+          <a onClick={Sobrenosotros}>Sobre nosotros</a>
           <a href="">Ubicaciones</a>
         </nav>
         <button className='BtnHeader'>Iniciar sesion</button>
