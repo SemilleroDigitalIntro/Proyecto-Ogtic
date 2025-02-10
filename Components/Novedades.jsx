f2384170b600ba193a6904c780e26ffbbd1ccce8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-10 12:32:12 -0400

    Se agrego el componente Novedades.

---
+++ Components/Novedades.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import DataNovedades from './Data/DataNovedades';
+import './Style/Novedades.css';
+
+
+export default function Novedades() {
+
+
+  const NovedadesD = (props) =>{
+    return(
+      <div className='Novedades__Cards'>
+        <img src={props.img} alt={props.imgAlt} />
+        <div className='NvModal'>
+          <h4>{props.title}</h4>
+          <p>{props.descripcion}</p>
+        </div>
+      </div>
+    )
+  };
+
+  const Novedadelist = DataNovedades.map( DN =>{
+    return(
+      <NovedadesD img = {DN.img} imgAlt = {DN.imgAlt} title = {DN.title}/>
+    )
+  })
+
+
+  return (
+    <section className='Novedades'>
+      <div className='Novedades__div1'>
+        <h3>Novedades</h3>
+        <a href="">Ver todas las novedades<i class='bx bx-right-arrow-alt'></i></a>
+      </div>
+      <div className='Novedades__div2'>
+        {Novedadelist}
+      </div>
+    </section>
+  )
+};
