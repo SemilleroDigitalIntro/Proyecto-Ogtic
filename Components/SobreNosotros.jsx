a3975752b9347a251e0b158a9601f9bbeae4381f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-10 12:32:57 -0400

    Se agrego el componente sobre nosotros.

---
+++ Components/SobreNosotros.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Style/SobreNosotros.less';
+
+export default function SobreNosotros() {
+  return (
+    <section className='SobreNosotros'>
+      <div className='SobreNosotros__div1'>
+        <img src="https://img.mmc.com.do/elcaribe-bucket/uploads/2024/05/6659dae064063_07a812e8-682f-4c3a-96f9-bd5e55e1b93b.jpg.webp" alt="Imagen GOB" />
+      </div>
+      <div  className='SobreNosotros__div2'>
+        <h2>Sobre Nosotros</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+           aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+             sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </section>
+  )
+}
