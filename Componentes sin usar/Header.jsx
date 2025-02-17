36d8875a2ea6ce8124bb20d30b53abf8fbb89762
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-17 12:44:18 -0400

    Update 2

--- Componentes sin usar/Header.jsx
+++ Componentes sin usar/Header.jsx
@@ -26,24 +26,11 @@ export default function Header() {
   };
   
   const Login = () =>{
-
-
-   if(ShowLogin == undefined){
-    document.getElementById('Login').style.display = 'flex' ;
-    ShowLogin = 1;
-   }else if(ShowLogin == 1){
-    document.getElementById('Login').style.display = 'none';
-    ShowLogin = 2;
-   }else if(ShowLogin == 2){
-    document.getElementById('Login').style.display = 'flex';
-    ShowLogin = 1;
-   }else{
-    alert('existe un error')
-   };
+    window.location.href = '/';
   };
 
   const Registrar = () => {
-
+    window.location.href = '/Registro';
   };
 
   return (
