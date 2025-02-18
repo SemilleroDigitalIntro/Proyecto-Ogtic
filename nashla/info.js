07e53c81ef119cd5a19320130224265353cfc38e
Nalaasg <nashlamissel27@gmail.com>
2025-02-18 09:28:11 -0400

    data cards serv. mas consultados
    descripcion acerca de lo servicios mas consultados

--- nashla/info.js
+++ nashla/info.js
@@ -78,3 +78,64 @@ const DataPuntosGob6 = [{
         Servicios:'57 Servicios',
         Instituciones: '14  Instituciones',
 }];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//INFO CARDS SERVICIOS MAS CONSULTADOS
+const DataServicios0 = [{
+        NameService: 'Registro de Obras Musicales con o sin Letra',
+        DetailService:'La Oficina Nacional de Derecho de Autor (ONDA), por esta vía ofrece la tramitación de solicitudes de inscripción de Obras Musicales con letra o sin ella.'
+}];
+const DataServicios1 = [{
+        NameService:'Consulta para Ciudadanos' ,
+        DetailService:'Acción mediante la cual, los ciudadanos podrán hacer consultas de sus beneficios y su perfil familiar en general.',
+}];
+
+const DataServicios2 = [{
+        NameService: 'Expediente Único Educativo',
+        DetailService:'Es una plataforma tecnológica que centraliza toda la información académica de los ciudadanos, desde el nivel básico hasta nivel superior.',
+}];
+
+const DataServicios3 = [{
+        NameService: 'Renovación licencia de conducir categoría 02',
+        DetailService:'Es la renovación de las licencias de conducir vencida categoría 02 para vehículos de transmisión mecánica y / o automática.',
+}];
+
+const DataServicios4 = [{
+        NameService: 'Solicitudes de Número de Seguridad Social (NSS)',
+        DetailService:'Gestión de la asignación del Número de Seguridad Social (NSS) a personas mayores de edad a los fines de que pueda ser inscrito en la base de datos',
+}];
+
+const DataServicios5 = [{
+        NameService: 'Renovación Pasaporte Adulto/Menor',
+        DetailService:'Esta categoría incluye a todo ciudadano dominicano, mayor de edad, quienes poseen un pasaporte dominicano',
+}];
+
+const DataServicios6 = [{
+        NameService: 'Permiso de Salida de Menores de Edad',
+        DetailService:'Es la certificación en la que un padre o ambos autorizan a un menor dominicano, a salir del territorio nacional. Hoja certificada con validez de 90 días.',
+}];
+
+
+
+
+
+
+
+
+
+
+
+
