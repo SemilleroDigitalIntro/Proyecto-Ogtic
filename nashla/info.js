e9593833668be49991a4cfc45a40b6e19fe3f609
Nalaasg <nashlamissel27@gmail.com>
2025-02-17 20:33:00 -0400

    informacion acerca de cada punto gob

---
+++ nashla/info.js
@@ -0,0 +1,80 @@
+
+//GOB Sambil
+const DataPuntosGob = [
+    {
+        img__ubi: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/04/c9/c4/sambil-santo-domingo.jpg?w=1200&h=-1&s=1',
+        img__ubi_descripcion:'Imagen del centro comercial Sambil',
+        NombrePuntoGob:'Punto GOB Sambil',
+        UbicacionPuntoGob:'Av. John F. Kennedy, Santo Domingo',
+        HorarioSemana:'Lunes a Viernes 8:00am - 8:00pm',
+        HorarioFinSemana:'Sabados 9:00am - 6:00pm',
+        Servicios:'16 Servicios',
+        Instituciones:'17 Instituciones',
+    }
+]
+
+
+//GOB MEGACENTRO
+const DataPuntosGob2 = [{
+        img__ubi: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/57/34/96/photo0jpg.jpg?w=1200&h=-1&s=1',
+        img__ubi_descripcion:'Imagen del centro comercial Megacentro',
+        NombrePuntoGob:'Punto GOB Megacentro',
+        UbicacionPuntoGob:'Av. San Vicente de Paúl 321, Santo Domingo Este',
+        HorarioSemana:'Lunes a Viernes 8:00am - 8:00pm',
+        HorarioFinSemana:'Sabados 9:00am - 6:00pm',
+        Servicios:'37 Servicios',
+        Instituciones: '13  Instituciones',
+}];
+
+
+
+//GOB EXPRESO
+const DataPuntosGob3 = [{
+        img__ubi: 'https://pgr.gob.do/wp-content/uploads/2022/05/LA2A1422.jpg',
+        img__ubi_descripcion:'Imagen del Punto GOB Expreso',
+        NombrePuntoGob:'Punto GOB Expreso',
+        UbicacionPuntoGob:'Parada 57 Av. Bulevard Del Faro PARQ ESTE II, Santo Domingo Este 11604',
+        HorarioSemana:'Lunes a Viernes 8:00am - 5:00pm',
+        HorarioFinSemana:'none',
+        Servicios:'61 Servicios',
+        Instituciones: '8  Instituciones',
+}];
+
+
+//GOB SANTIAGO
+const DataPuntosGob4 = [{
+        img__ubi: 'https://wp.ogtic.gob.do/wp-content/uploads/2024/09/PuntoGOB-blog.jpeg',
+        img__ubi_descripcion:'Imagen del Punto GOB Santiago',
+        NombrePuntoGob:'Punto GOB Santiago',
+        UbicacionPuntoGob:'Av. Salvador Estrella Sadhalá, Santiago de los Caballeros 11302',
+        HorarioSemana:'Lunes a Viernes 8:00am - 8:00pm',
+        HorarioFinSemana:'Sabados 9:00am - 6:00pm',
+        Servicios:'36 Servicios',
+        Instituciones: '16  Instituciones',
+}];
+
+
+//GOB SAN CRISTOBAL
+const DataPuntosGob5 = [{  
+        img__ubi: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn9lI8oV5uXbEt8XtTIokZNGPte4TU1DoaKQ&s',
+        img__ubi_descripcion:'Imagen del Punto GOB San Cristobal',
+        NombrePuntoGob:'Punto GOB San Cristobal',
+        UbicacionPuntoGob:'El Bravo de la C/ El Esfuerzo 3, San Cristóbal 91000',
+        HorarioSemana:'Lunes a Viernes 8:00am - 8:00pm',
+        HorarioFinSemana:'Sabados 9:00am - 6:00pm',
+        Servicios:'24 Servicios',
+        Instituciones: '11  Instituciones',
+}];
+
+
+//GOB COLINA CENTRO
+const DataPuntosGob6 = [{  
+        img__ubi: 'https://sie.gob.do/wp-content/uploads/2023/12/406798511_18401898547027429_1564035571578195795_n-1024x683.jpg',
+        img__ubi_descripcion:'Imagen del Punto GOB Colina Centro',
+        NombrePuntoGob:'Punto GOB Colina Centro',
+        UbicacionPuntoGob:'Av.  Jacobo Majluta, Santo Domingo',
+        HorarioSemana:'Lunes a Viernes 8:00am - 8:00pm',
+        HorarioFinSemana:'Sabados 9:00am - 6:00pm',
+        Servicios:'57 Servicios',
+        Instituciones: '14  Instituciones',
+}];
