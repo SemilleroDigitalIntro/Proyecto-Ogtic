d292ecad486681d1645844b85d5014823f5f1b40
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-19 13:22:44 -0400

    Estilo del buscador completo

--- Componentes sin usar/component_B.jsx
+++ Componentes sin usar/component_B.jsx
@@ -3,18 +3,21 @@ import './Style/Component_B.less'
 
 export default function Component_B() {
 
+    const Categoria = () => {
+
+    };
 
   return (
     <form className='Buscador'>
         <div className='InBuscador'>
         <div className='Buscador_div1'>
-            <button>Categoria<i class='bx bx-chevron-right'></i></button>
+            <button onClick={Categoria}>Categoria<i class='bx bx-chevron-right'></i></button>
             <div className='Buscador_categorias'>
                 
             </div>
         </div>
         <div className='Buscador_div2'>
-            <input type="text" name="" id="Buscador" required/>
+            <input type="text" name="" id="Buscador" placeholder='Buscar Ej.: Solicitud de Pasaporte por primera vez' required/>
         </div>
         <div className='Buscador_div3'>
             <button type="submit"><i class='bx bx-search-alt-2' ></i></button>
