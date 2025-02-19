48b274788ea9a994d9ae02bb1cd8ecc954680c4c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-19 12:39:03 -0400

    Update

--- Componentes sin usar/Structura.jsx
+++ Componentes sin usar/Structura.jsx
@@ -8,6 +8,7 @@ import Footer from './Footer';
 import Centros from './Centros';
 import ServMasCon from './servMasCon';
 import InfoAdicional from './InfoAdicional';
+import Component_B from './component_B';
 
 
 export default function Structura() {
@@ -15,6 +16,7 @@ export default function Structura() {
     <section>
         <Header/>
         <Smain/>
+        <Component_B/>
         <ServMasCon/>
         <Centros/>
         <InfoAdicional/>
---
+++ Componentes sin usar/component_B.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Style/Component_B.less'
+
+export default function Component_B() {
+
+
+  return (
+    <form className='Buscador'>
+        <div className='InBuscador'>
+        <div className='Buscador_div1'>
+            <button>Categoria<i class='bx bx-chevron-right'></i></button>
+            <div className='Buscador_categorias'>
+                
+            </div>
+        </div>
+        <div className='Buscador_div2'>
+            <input type="text" name="" id="Buscador" required/>
+        </div>
+        <div className='Buscador_div3'>
+            <button type="submit"><i class='bx bx-search-alt-2' ></i></button>
+        </div>
+        </div>
+    </form>
+  )
+};
