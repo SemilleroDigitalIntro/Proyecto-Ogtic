218e0212230d1a837262c2b15c851b1cdd7d1404
Nalaasg <nashlamissel27@gmail.com>
2025-02-19 09:20:41 -0400

    Inst Occidental Mall

--- Components/Data/DataInst.js
+++ Components/Data/DataInst.js
@@ -1,6 +1,31 @@
 const GobOccidental = [{
-    Instituciones : ''
-}];
+    Instituciones : 'MESCYT',
+},
+{
+    Instituciones : 'Superintendencia de Electricidad (SIE)'
+},
+{
+    Instituciones : 'Dirección General de Información y Defensa de los Afiliados a la Seguridad Social (DIDA)',
+},
+{
+    Instituciones : 'Superintendencia de Salud y Riesgos Laborales (SISALRIL)',
+},
+{
+    Instituciones : 'Tesorería de la Seguridad Social (TSS)',
+},
+{
+    Instituciones : 'Dirección General de Medicamentos, Alimentos y Productos Sanitarios',
+},
+{
+    Instituciones : 'Ministerio de Educación de la República Dominicana',
+},
+{
+    Instituciones : 'Instituto Nacional de Tránsito y Transporte Terrestre (INTRANT)',
+},
+{
+    Instituciones : 'Dirección General de Migración (DGM)'
+},
+];
 const GobSambil = [{
     Instituciones : ''
 }];
