a5787231e6a9d09fef46e0aeb5a4d6513676901e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-20 12:38:54 -0400

    Error de informacion corregido.

--- Components/ComponentesParaF/SeleccionPuntoGOB.jsx
+++ Components/ComponentesParaF/SeleccionPuntoGOB.jsx
@@ -12,9 +12,9 @@ export default function SeleccionPuntoGOB() {
         DeployPuntosGB ? document.getElementById('PuntoGOB__GOB_Arrow').style.rotate = '90deg' : document.getElementById('PuntoGOB__GOB_Arrow').style.rotate = '0deg';
     };
 
-    const PGOB_megacentro = () => {
+    const PGOB_Santo_Domingo_Oeste = () => {//OccidentalMall
         let PuntoGOB__GOB = document.getElementById('PuntoGOB__GOB');
-        PuntoGOB__GOB.innerText = 'Punto GOB Megacentro';
+        PuntoGOB__GOB.innerText = 'Punto GOB Occidental';
         PuntoGOBSelecionado = 1;
         SetdeployGB(!DeployPuntosGB);
         document.querySelector('.PuntoGOB__span').style.display = 'none';
@@ -28,34 +28,45 @@ export default function SeleccionPuntoGOB() {
         document.querySelector('.PuntoGOB__span').style.display = 'none';
         document.getElementById('PuntoGOB__GOB_Arrow').style.rotate = '0deg';
     };
-    const PGOB_Santo_Domingo_Oeste = () => {
+    const PGOB_Expreso = () => {
         let PuntoGOB__GOB = document.getElementById('PuntoGOB__GOB');
-        PuntoGOB__GOB.innerText = 'Punto GOB Santo Domingo Oeste';
+        PuntoGOB__GOB.innerText = 'Punto GOB Expreso';
         PuntoGOBSelecionado = 3;
         SetdeployGB(!DeployPuntosGB);
         document.querySelector('.PuntoGOB__span').style.display = 'none';
         document.getElementById('PuntoGOB__GOB_Arrow').style.rotate = '0deg';
     };
-    const PGOB_Santiago = () => {
+    const PGOB_megacentro = () => {
         let PuntoGOB__GOB = document.getElementById('PuntoGOB__GOB');
-        PuntoGOB__GOB.innerText = 'Punto GOB Santiago';
+        PuntoGOB__GOB.innerText = 'Punto GOB Megacentro';
         PuntoGOBSelecionado = 4;
         SetdeployGB(!DeployPuntosGB);
         document.querySelector('.PuntoGOB__span').style.display = 'none';
         document.getElementById('PuntoGOB__GOB_Arrow').style.rotate = '0deg';
     };
-    const PGOB_Santo_Doming_Norte = () => {
+   
+    
+    const PGOB_Santiago = () => {
         let PuntoGOB__GOB = document.getElementById('PuntoGOB__GOB');
-        PuntoGOB__GOB.innerText = 'Punto GOB Santo Domingo Norte';
+        PuntoGOB__GOB.innerText = 'Punto GOB Santiago';
         PuntoGOBSelecionado = 5;
         SetdeployGB(!DeployPuntosGB);
         document.querySelector('.PuntoGOB__span').style.display = 'none';
         document.getElementById('PuntoGOB__GOB_Arrow').style.rotate = '0deg';
     };
+   
     const PGOB_San_Cristóbal = () => {
         let PuntoGOB__GOB = document.getElementById('PuntoGOB__GOB');
         PuntoGOB__GOB.innerText = 'Punto GOB San Cristóbal';
         PuntoGOBSelecionado = 6;
+        SetdeployGB(!DeployPuntosGB);
+        document.querySelector('.PuntoGOB__span').style.display = 'none';
+        document.getElementById('PuntoGOB__GOB_Arrow').style.rotate = '0deg';
+    };
+    const PGOB_Santo_Doming_Norte = () => {//Colina Centro
+        let PuntoGOB__GOB = document.getElementById('PuntoGOB__GOB');
+        PuntoGOB__GOB.innerText = 'Punto GOB Colina Centro';
+        PuntoGOBSelecionado = 7;
         SetdeployGB(!DeployPuntosGB);
         document.querySelector('.PuntoGOB__span').style.display = 'none';
         document.getElementById('PuntoGOB__GOB_Arrow').style.rotate = '0deg';
@@ -67,12 +78,14 @@ export default function SeleccionPuntoGOB() {
         <span>Punto GOB de preferencia</span>
         <button onClick={DeployPuntosGOB} className='PuntoGOB__GOB'><span id='PuntoGOB__GOB'>Seleciona un punto GOB</span><i id='PuntoGOB__GOB_Arrow' class='bx bx-chevron-right' ></i></button>
         <div className='PuntoGOB__span' id='PuntoGOB__span'>
-            <span className='exection' onClick={PGOB_megacentro}>Punto GOB Megacentro</span>
+            <span className='exection' onClick={PGOB_Santo_Domingo_Oeste}>Punto GOB Occidental</span>
             <span onClick={PGOB_Sambil} >Punto GOB Sambil</span>
-            <span onClick={PGOB_Santo_Domingo_Oeste}>Punto GOB Occidental</span>
+            <span onClick={PGOB_Expreso} >Punto GOB Expreso</span>
+            <span  onClick={PGOB_megacentro}>Punto GOB Megacentro</span>
             <span onClick={PGOB_Santiago}>Punto GOB Santiago</span>
-            <span onClick={PGOB_Santo_Doming_Norte}>Punto GOB Colina Centro</span>
-            <span className='exection2' onClick={PGOB_San_Cristóbal}>Punto GOB San Cristobal</span>
+            <span  onClick={PGOB_San_Cristóbal}>Punto GOB San Cristobal</span>
+            <span className='exection2' onClick={PGOB_Santo_Doming_Norte}>Punto GOB Colina Centro</span>
+            
         </div>
     </div>
   )
