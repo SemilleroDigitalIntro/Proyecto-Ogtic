76dd07a295fd7bef47c983b22d27cdd5dce4caba
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-20 12:40:38 -0400

    Se agrego el array de datos de servicios por institucion y punto Gob.

---
+++ Components/Data/DataServicios.js
@@ -0,0 +1,152 @@
+const DataServicios = [
+    //OcidentalMall
+    {
+        MESCYT: [
+            {
+                ServiciosM: 'Legalización de Documentos Académicos Emitidos por las IES Nacionales Activas'
+            }
+        ],
+        SIE:[
+            {
+                ServiciosSIE: 'Reclamaciones en Tarifas BTS1 y BTS2'
+            }
+        ],
+        DIDA:[
+            {
+                ServiciosDIDA: 'No hay servicios Disponibles'
+            }
+        ],
+        SISALRIL:[
+            {
+                ServiciosSISALRIL: 'Atención a Peticiones, Quejas, Reclamos y Sugerencias'
+            },
+            {
+                ServiciosSISALRIL: 'Investigación de Traspasos por Alto Costo y/o Cirugía Pendiente'
+            },
+            {
+                ServiciosSISALRIL: 'Respuesta a las Solicitudes y Casos (Revisión de cuenta clínica)'
+            },
+            {
+                ServiciosSISALRIL: 'Respuesta a la solicitud de negación de cobertura '
+            },
+            {
+                ServiciosSISALRIL: 'Investigación de Afiliación y/o Traspaso irregular'
+            }
+        ],
+        TSS: [
+            {
+                ServiciosTSS: 'Información de Reclamaciones'
+            }
+        ],
+        DMAPS:[
+            {
+                ServiciosDMAPS: 'Certificado Libre Venta para Medicamentos'
+            }
+        ],
+        MINE:[
+            {
+                ServiciosMine: 'Certificación de Exequátur'
+            }
+        ],
+        INTRANT: [
+            {
+                ServiciosINTRANT: 'Renovación licencia de conducir categoría 01'
+            },
+            {
+                ServiciosINTRANT: 'Renovación licencia de conducir categoría 02'
+            },
+            {
+                ServiciosINTRANT: 'Renovación licencia de conducir categoría 03'
+            }
+        ],
+        DGM: [
+            {
+                ServiciosDGM: 'Autogate'
+            }
+        ],
+        
+
+    },
+    //Sambil
+    {
+        DIDA:[
+            {
+                ServiciosDIDA:'Corrección de datos personales en la base de datos del Sistema Dominicano de Seguridad Social (SDSS)'
+            },
+            {
+                ServiciosDIDA:'Solicitud de Número de Seguridad Social (NSS)'
+            },
+            {
+                ServiciosDIDA:'Atención de reclamaciones en el SDSS'
+            },
+            {
+                ServiciosDIDA:'Consultas y entrega de respuestas sobre el SDSS'
+            },
+            {
+                ServiciosDIDA:'Solicitud de historial de aportes en el SDSS'
+            },
+            {
+                ServiciosDIDA:'Solicitud de constancia de afiliación en el Seguro Familiar de Salud y de afiliación en el Seguro de Pensiones.'
+            },
+            
+        ],
+        PN:[
+            {
+                ServicioPN: 'Certificación de pérdida de documentos y objetos'
+            }
+        ],
+        DGP:[
+            {
+                ServicioDGP: 'Renovación de pasaporte adulto por pérdida.'
+            },
+            {
+                ServicioDGP: 'Renovación de pasaporte adulto por deterioro.'
+            },
+            {
+                ServicioDGP: 'Renovación de pasaporte menor por pérdida.'
+            },
+            
+        ],
+        DGM:[
+            {
+                ServiciosDGM: 'Autogate'
+            }
+        ],
+        TSS:[
+            {
+                ServiciosTSS: 'Información de reclamaciones relacionadas con normalización de RNC, nóminas de trabajadores y períodos omisos.'
+            }
+        ],
+
+    },
+    //Expreso
+    {
+        ADESS: [
+            {
+                ServiciosADESS: 'Consulta para ciudadanos sobre beneficios y perfil familiar.'
+            }
+        ],
+        ASDE: [
+            {
+                ServiciosASDE: 'Información general.'
+            },
+            {
+                ServiciosASDE: 'Registro civil para legalización de contratos de venta de muebles y financiamiento de vehículos.'
+            }
+        ],
+        DGJP: [
+            {
+                ServiciosDGJP: 'Pensión por antigüedad'
+            },
+            {
+                ServiciosDGJP: 'Inclusiones a nómina de pensiones'
+            }
+        ],
+        DGP:[
+            {
+                ServicioDGP: 'Renovación de pasaporte para adultos por vencimiento.'
+            }
+        ]
+    },
+];
+export default DataServicios;
