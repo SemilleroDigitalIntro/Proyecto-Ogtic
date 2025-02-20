e62c5dc768b06a609355f75dd7bac78e95b2c4c4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-20 12:38:30 -0400

    Bug de corregido seleccion

--- Components/ComponentesParaF/SeleccionInstitucion.jsx
+++ Components/ComponentesParaF/SeleccionInstitucion.jsx
@@ -26,9 +26,10 @@ export default function SeleccionInstitucion() {
 
               for(let I = 0 ; I < 14; I++){
                 let Ints = document.createElement('span');
-                Ints.className = `${DataPuntoGobs[0].GobOcidentalMall[I].Instituciones}`;
+                Ints.className = `Servicio${I}`;
                 Ints.addEventListener('click',()=>{
                   Institucion__GOB.innerText = `${DataPuntoGobs[0].GobOcidentalMall[I].Instituciones}`;
+                  InstitucionSeleccionada = I;
                 })
                 let Intstext = document.createTextNode(`${DataPuntoGobs[0].GobOcidentalMall[I].Instituciones}`);
                 
@@ -44,6 +45,7 @@ export default function SeleccionInstitucion() {
                 Ints.className = `${DataPuntoGobs[0].GobSambil[I].Instituciones}`;
                 Ints.addEventListener('click',()=>{
                   Institucion__GOB.innerText = `${DataPuntoGobs[0].GobSambil[I].Instituciones}`;
+                  InstitucionSeleccionada = I;
                 })
                 let Intstext = document.createTextNode(`${DataPuntoGobs[0].GobSambil[I].Instituciones}`);
                 
@@ -60,6 +62,8 @@ export default function SeleccionInstitucion() {
                 Ints.className = `${DataPuntoGobs[0].GobExpreso[I].Instituciones}`;
                 Ints.addEventListener('click',()=>{
                   Institucion__GOB.innerText = `${DataPuntoGobs[0].GobExpreso[I].Instituciones}`;
+                  InstitucionSeleccionada = I;
+
                 })
                 let Intstext = document.createTextNode(`${DataPuntoGobs[0].GobExpreso[I].Instituciones}`);
                 
@@ -74,6 +78,7 @@ export default function SeleccionInstitucion() {
                 Ints.className = `${DataPuntoGobs[0].GobMegacentro[I].Instituciones}`;
                 Ints.addEventListener('click',()=>{
                   Institucion__GOB.innerText = `${DataPuntoGobs[0].GobMegacentro[I].Instituciones}`;
+                  InstitucionSeleccionada = I;
                 })
                 let Intstext = document.createTextNode(`${DataPuntoGobs[0].GobMegacentro[I].Instituciones}`);
                 
@@ -87,6 +92,7 @@ export default function SeleccionInstitucion() {
                 Ints.className = `${DataPuntoGobs[0].GobSantiago[I].Instituciones}`;
                 Ints.addEventListener('click',()=>{
                   Institucion__GOB.innerText = `${DataPuntoGobs[0].GobSantiago[I].Instituciones}`;
+                  InstitucionSeleccionada = I;
                 })
                 let Intstext = document.createTextNode(`${DataPuntoGobs[0].GobSantiago[I].Instituciones}`);
                 
@@ -100,6 +106,7 @@ export default function SeleccionInstitucion() {
                 Ints.className = `${DataPuntoGobs[0].GobSanCristobal[I].Instituciones}`;
                 Ints.addEventListener('click',()=>{
                   Institucion__GOB.innerText = `${DataPuntoGobs[0].GobSanCristobal[I].Instituciones}`;
+                  InstitucionSeleccionada = I;
                 })
                 let Intstext = document.createTextNode(`${DataPuntoGobs[0].GobSanCristobal[I].Instituciones}`);
                 
@@ -112,6 +119,7 @@ export default function SeleccionInstitucion() {
                 Ints.className = `${DataPuntoGobs[0].GobColinaCentro[I].Instituciones}`;
                 Ints.addEventListener('click',()=>{
                   Institucion__GOB.innerText = `${DataPuntoGobs[0].GobColinaCentro[I].Instituciones}`;
+                  InstitucionSeleccionada = I;
                 })
                 let Intstext = document.createTextNode(`${DataPuntoGobs[0].GobColinaCentro[I].Instituciones}`);
                 
@@ -126,43 +134,43 @@ export default function SeleccionInstitucion() {
 
     };
 
-    const Institucion_DGP = () => {
-      document.getElementById('Institucion__GOB').innerText = 'Dirección General de Pasaportes';
-      document.getElementById('Institucion__GOB_Arrow').style.rotate = '0deg';
-      document.getElementById('Institucion__span').style.display = 'none' ;
-      SetdeployI(!DeployI);
-      InstitucionSeleccionada = 1;
+    // const Institucion_DGP = () => {
+    //   document.getElementById('Institucion__GOB').innerText = 'Dirección General de Pasaportes';
+    //   document.getElementById('Institucion__GOB_Arrow').style.rotate = '0deg';
+    //   document.getElementById('Institucion__span').style.display = 'none' ;
+    //   SetdeployI(!DeployI);
+    //   InstitucionSeleccionada = 1;
 
-    };
+    // };
 
-    const Institucion_Intrant = () => {
-      document.getElementById('Institucion__GOB').innerText = 'Intrant';
-      document.getElementById('Institucion__GOB_Arrow').style.rotate = '0deg';
-      document.getElementById('Institucion__span').style.display = 'none' ;
-      SetdeployI(!DeployI);
-      InstitucionSeleccionada = 2;
-    };
+    // const Institucion_Intrant = () => {
+    //   document.getElementById('Institucion__GOB').innerText = 'Intrant';
+    //   document.getElementById('Institucion__GOB_Arrow').style.rotate = '0deg';
+    //   document.getElementById('Institucion__span').style.display = 'none' ;
+    //   SetdeployI(!DeployI);
+    //   InstitucionSeleccionada = 2;
+    // };
 
-    const Institucion_JCE = () => {
-      document.getElementById('Institucion__GOB').innerText = 'Junta central electoral';
-      document.getElementById('Institucion__GOB_Arrow').style.rotate = '0deg';
-      document.getElementById('Institucion__span').style.display = 'none' ;
-      SetdeployI(!DeployI);
-      InstitucionSeleccionada = 3;
-    };
+    // const Institucion_JCE = () => {
+    //   document.getElementById('Institucion__GOB').innerText = 'Junta central electoral';
+    //   document.getElementById('Institucion__GOB_Arrow').style.rotate = '0deg';
+    //   document.getElementById('Institucion__span').style.display = 'none' ;
+    //   SetdeployI(!DeployI);
+    //   InstitucionSeleccionada = 3;
+    // };
 
-    const Institucion_DGM = () => {
-      document.getElementById('Institucion__GOB').innerText = 'Direccion General de Migracion';
-      document.getElementById('Institucion__GOB_Arrow').style.rotate = '0deg';
-      document.getElementById('Institucion__span').style.display = 'none' ;
-      SetdeployI(!DeployI);
-      InstitucionSeleccionada = 4;
-    };
+    // const Institucion_DGM = () => {
+    //   document.getElementById('Institucion__GOB').innerText = 'Direccion General de Migracion';
+    //   document.getElementById('Institucion__GOB_Arrow').style.rotate = '0deg';
+    //   document.getElementById('Institucion__span').style.display = 'none' ;
+    //   SetdeployI(!DeployI);
+    //   InstitucionSeleccionada = 4;
+    // };
 
   return (
     <div className='Institucion'>
         <span>Institución pública</span>
-        <button onClick={DeployInstitucion} className='Institucion__GOB'><span id=''>Seleciona una Institucion</span><i id='Institucion__GOB_Arrow' class='bx bx-chevron-right' ></i></button>
+        <button onClick={DeployInstitucion} className='Institucion__GOB'><span id='Institucion__GOB'>Seleciona una Institucion</span><i id='Institucion__GOB_Arrow' class='bx bx-chevron-right' ></i></button>
         <div className='Institucion__span' id='Institucion__span'>
             
         </div>
